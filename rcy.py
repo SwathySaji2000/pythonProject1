dd10a5bfa00a3885c6126378e9ddd2cbc6a49681
SwathySaji2000 <120879444+SwathySaji2000@users.noreply.github.com>
2023-01-19 08:27:25 -0800

    Add files via upload

---
+++ rcy.py
@@ -0,0 +1,7 @@
+def area(l,b):
+    area=l*b
+    print("Area is",area)
+
+def perimeter(l,b):
+    perimeter=2*(l+b)
+    print("Perimeter is",perimeter)
---
+++ sq.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a number"))
+fact=1
+if(n<0):
+    print("factorial does not exist")
+if(n==0)
